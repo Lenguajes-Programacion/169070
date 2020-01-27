2aac0fb66089b6e1c1a943896648ae068682faf9
Bryan Galvan <60107816+Otbi@users.noreply.github.com>
2020-01-27 12:31:43 -0700

    loquesea

---
+++ clase_1/Practica_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace practicauno
+{
+    class Program
+    {
+        //variable de la clase
+        static string nombre = "bryan";
+        static void Main(string[] args)
+        {
+            //respondemos el primer argumento
+            //condicion se debe cumplir todas las sentencias si utilizamos &&
+            //condicion se debe de cumplir cualquier da las dos sentencias
+            //tipos de comparacion: > < == >= <=
+            if (args[0].Length > 0 && args[0] == nombre)
+            {
+                Console.WriteLine($"Hello { args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("usuario no permitido");
+            }
+            Console.ReadKey();
+        }
+    }
+}
