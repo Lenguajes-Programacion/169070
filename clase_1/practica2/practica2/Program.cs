3fabba505819539b1de32e308e71b71d7e5e80b7
Bryan Galvan <60107816+Otbi@users.noreply.github.com>
2020-02-07 12:54:34 -0700

    calculadora

---
+++ clase_1/practica2/practica2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace practica2
+{
+    class Program
+    {
+           //proyecto calculadora
+           //se espera recibir los argumentos valor1 (int) valor2 (string)
+           //valor3 (int)
+           //responder resultado en (int)
+        static void Main(string[] args)
+        {
+            switch (args [1])
+                {
+                case "+":
+                    Console.WriteLine ("es una suma");
+                    break;
+
+                    case "-":
+                    Console.WriteLine ("es una resta");
+                    break;
+
+                    case "*":
+                    Console.WriteLine ("es una multiplicacion");
+                    break;
+
+                    case "/":
+                    Console.WriteLine ("es una divison");
+                    break;
+
+                default:
+                    Console.WriteLine ("los argumentos no cuentan con un operador valido:" + args[1]);
+                    break;
+            }
+        }
+    }
+}
