32e8eb7e1d8bea5012c7fa2460c6ad8b5458251c
Bryan Galvan <60107816+Otbi@users.noreply.github.com>
2020-02-10 11:26:59 -0700

    calculadora tarea

--- clase_1/practica2/practica2/Program.cs
+++ clase_1/practica2/practica2/Program.cs
@@ -1,37 +1,51 @@
 ﻿using System;
-
-namespace practica2
+namespace calculadora1
 {
     class Program
     {
-           //proyecto calculadora
-           //se espera recibir los argumentos valor1 (int) valor2 (string)
-           //valor3 (int)
-           //responder resultado en (int)
         static void Main(string[] args)
         {
-            switch (args [1])
-                {
-                case "+":
-                    Console.WriteLine ("es una suma");
+            string operador;
+            int suma, resta, multi, divi, digito1, digito2;
+            Console.WriteLine("Realiza una operacion", System.Environment.NewLine, "ingresa el primer digito");
+            Console.WriteLine("escribe el primer digito");
+            digito1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("escribe un operador");
+            operador = Console.ReadLine();
+            Console.WriteLine("escribe el segundo digito");
+            digito2 = Int32.Parse(Console.ReadLine());
+
+            suma = digito1 + digito2;
+            resta = digito1 - digito2;
+           multi = digito1 * digito2;
+            divi = digito1 / digito2;
+
+
+            switch (operador)
+            {
+                case "+" :
+                    Console.WriteLine("tu operacion es una suma y el resultado es: " + suma);
                     break;
 
-                    case "-":
-                    Console.WriteLine ("es una resta");
+                case "-":
+                    Console.WriteLine("tu operacion es una restay el resultado es: " + resta);
                     break;
 
-                    case "*":
-                    Console.WriteLine ("es una multiplicacion");
+                case "*":
+                    Console.WriteLine("tu operacion es una multiplicacion y el resultado es: " + multi);
                     break;
 
-                    case "/":
-                    Console.WriteLine ("es una divison");
+                case "/":
+                    Console.WriteLine("tu operacion es una divison y el resultado es: " + divi);
                     break;
 
                 default:
-                    Console.WriteLine ("los argumentos no cuentan con un operador valido:" + args[1]);
+                    Console.WriteLine("los argumentos no cuentan con un operador valido:" + operador);
                     break;
+
+
             }
+            Console.ReadLine();
         }
     }
 }
