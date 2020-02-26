8a02ffcc80ea3261891037ee79465c7fa67c9982
Bryan Galvan <60107816+Otbi@users.noreply.github.com>
2020-02-26 12:36:35 -0700

    idk

---
+++ Memoria/Practicamemoria/Practicamemoria/pmemo.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace calculadora1
+{
+    class Memoria
+    {
+        public void memo()
+        {
+            string archivoDB = "../../../db.json";
+            StreamReader reader = new StreamReader(archivoDB);
+            var dbJSON = reader.ReadToEnd();
+            var dbObject = JObject.Parse(dbJSON);
+            // Prueba de lectura de archivo db.json
+            //var result = dbObject.ToString();
+            //var result = dbObject["arreglo"].ToString();
+            //var result = dbObject["arreglo"][0].ToString();
+            // Lectura de json iterable
+            foreach (var item in dbObject)
+            {
+                // Iteración individual de cada grupo de datos del objeto json.
+                Console.WriteLine("Dato en memoria:");
+                MemoriaData memoriaData = new MemoriaData(item.Key.ToString(), item.Value["operacion"].ToString(), item.Value["resultado"].ToString());
+                Console.WriteLine("{0} - {1}", memoriaData.fecha.ToLongDateString(),
+                memoriaData.fecha.ToLongTimeString());
+                Console.WriteLine(memoriaData.resultado.ToString());
+                Console.WriteLine("----------------- \n");
+            }
+        }
+    }
+    class MemoriaData
+    {
+        public DateTime fecha;
+        public String operacion;
+        public int resultado;
+
+        public MemoriaData(String date, String operation, String result)
+        {
+            fecha = DateTime.Parse(date);
+            operacion = operation;
+            resultado = int.Parse(result);
+        }
+    }
+}
+
